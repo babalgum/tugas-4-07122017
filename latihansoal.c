f98c94b6f46ec7530484b79b560f397f7bd7e3b3
Iqbal mohamad ayubi <32825973+babalgum@users.noreply.github.com>
2017-12-07 23:24:49 +0700

    Add files via upload

---
+++ latihansoal.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+	char nama[30];
+	char npm [30];
+	float IPK;
+	int nilai;
+	char kelamin [20];
+	bool status= true;
+	printf ("========Selamat Datang di Program Saya========== \n");
+	printf ("\n");
+	
+	printf ("Masukan Nama: ");
+	scanf ("%s", &nama);
+	
+	printf ("Masukan Npm: ");
+	scanf ("%s", &npm);
+	
+	printf ("Masukan IPK: ");
+	scanf ("%f", &IPK);
+	
+	printf ("Masukan Nilai: ");
+	scanf ("%d", &nilai);
+	
+	printf ("P/L: ");
+	scanf ("%s", &kelamin);
+	
+	printf ("Status Pernikahan :");
+	scanf ("%d", &status);
+	
+	printf ("\n");
+	printf ("==========data diri anda adalah======== \n");
+	printf ("\n");
+	
+	printf ("Nama anda adalah %s\n", nama);
+	printf ("Npm anda adalah %s\n", npm);
+	printf ("Ipk anda adalah %f\n", IPK);
+	printf ("Nilai anda adalah %d\n", nilai);
+	printf ("jenis kelamn anda adalah %s\n", kelamin);
+	printf ("status pernikahan anda %d\n", status);
+
+	return 0;
+}
